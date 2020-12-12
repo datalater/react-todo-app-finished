3093635ae1930bef73278c9f974e6ba64820daa8
datalater <the7mincheol@gmail.com>
2020-12-12 16:52:20 +0900

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+    </div>
+  )
+}
+
+export default App;
